1a01cbfb6c4d9af56569f16e2e3972aca866c7ee
IagoGoncalves <iago_375@hotmail.com>
2024-09-27 14:24:55 -0300

    responsive ready

--- wp-content/themes/efc/page-court.php
+++ wp-content/themes/efc/page-court.php
@@ -7,11 +7,11 @@
 get_header();
 ?>
 <main id="socialarea" class="socialarea" tabindex="-1" role="main">
-   <div class="container container-interno">
+   <div class="container-interno">
     <h1 class="title"><?php the_title(); ?></h1>
    </div>
    <figure><?php the_post_thumbnail(); ?></figure>
-   <div class="container container-interno">
+   <div class="container-interno">
       <h2 class="title"><?= get_field('titulo') ?></h2>
       <p class="paragraph"><?= get_field('descritivo') ?></p>
    </div>
